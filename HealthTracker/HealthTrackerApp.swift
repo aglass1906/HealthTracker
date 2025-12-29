2aad6c36e73bf8245481d575158592c17d546ce5
aglass <alan.glass.sankoa@gmail.com>
2025-12-29 15:01:28 -0500

    Initial Commit

---
+++ HealthTracker/HealthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerApp.swift
+//  HealthTracker
+//
+//  Created by Alan Glass on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
