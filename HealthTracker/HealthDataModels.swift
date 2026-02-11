f423851c111102eb9759b5b441c4426110af4e63
aglass <alan.glass.sankoa@gmail.com>
2026-02-11 08:56:02 -0500

    feat: Add new workout activity types and update the default display string.

--- HealthTracker/HealthDataModels.swift
+++ HealthTracker/HealthDataModels.swift
@@ -136,13 +136,52 @@ extension HKWorkoutActivityType {
         case .stairClimbing: return "Stair Climbing"
         case .dance: return "Dance"
         case .golf: return "Golf"
+        case .highIntensityIntervalTraining: return "HIIT"
+        case .functionalStrengthTraining: return "Functional Strength"
+        case .coreTraining: return "Core Training"
+        case .cooldown: return "Cooldown"
+        case .mindAndBody: return "Mind & Body"
+        case .flexibility: return "Flexibility"
+        case .barre: return "Barre"
+        case .pilates: return "Pilates"
+        case .jumpRope: return "Jump Rope"
+        case .kickboxing: return "Kickboxing"
         case .boxing: return "Boxing"
         case .martialArts: return "Martial Arts"
-        case .pilates: return "Pilates"
-        case .skatingSports: return "Skating"
-        case .snowSports: return "Snow Sports"
-        case .waterSports: return "Water Sports"
-        default: return "Other"
+        case .taiChi: return "Tai Chi"
+        case .wrestling: return "Wrestling"
+        
+        // Sports
+        case .pickleball: return "Pickleball"
+        case .tableTennis: return "Table Tennis"
+        case .racquetball: return "Racquetball"
+        case .squash: return "Squash"
+        case .badminton: return "Badminton"
+        case .volleyball: return "Volleyball"
+        case .handball: return "Handball"
+        case .lacrosse: return "Lacrosse"
+        case .rugby: return "Rugby"
+        case .softball: return "Softball"
+        case .trackAndField: return "Track & Field"
+        case .gymnastics: return "Gymnastics"
+        case .bowling: return "Bowling"
+        case .fencing: return "Fencing"
+        
+        // Water
+        case .waterFitness: return "Water Fitness"
+        case .waterPolo: return "Water Polo"
+        case .surfingSports: return "Surfing"
+        case .sailing: return "Sailing"
+        
+        // Cardio / Other
+        case .stepTraining: return "Step Training"
+        case .cardioDance: return "Cardio Dance"
+        case .socialDance: return "Social Dance"
+        case .stairs: return "Stairs"
+        case .wheelchairWalkPace: return "Wheelchair Walk"
+        case .wheelchairRunPace: return "Wheelchair Run"
+        
+        default: return "Workout"
         }
     }
 }
